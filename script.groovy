844462266100fb92332ed3bbfcaa211c99f3f32d
NahuelAquino2000 <89874257+NahuelAquino2000@users.noreply.github.com>
2021-08-31 17:59:15 -0300

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,9 @@
+def function() {
+    echo 'ejemplo'
+    echo "testing variables cruzadas ${VAR_TEST}"
+}
+
+return this //Esto es para importarlo al Jenkinsfile
+
+
+//desde acá puedo llamar variables del Jenkinsfile
